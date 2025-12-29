41654e90bad265b3f814a4f5fd84dcb2409c06fe
Vaishnavi Chandore <97340019+VaishnaviAnalyzed@users.noreply.github.com>
2025-12-29 20:17:44 +0530

    Add files via upload

---
+++ Ollama_model_using_Gemma_&_Deepseek/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from ollama import Client
+
+# ✅ Create client instance (VERY IMPORTANT)
+client = Client(host="http://localhost:11434")
+
+st.set_page_config(
+    page_title="Custom LLM model by Vaishnavi Chandore - Ollama",
+    layout="centered"
+)
+
+st.title("Ollama App")  # using gemma model
+
+prompt = st.text_area("Enter your prompt:", height=200)
+
+if st.button("Generate Response"):
+    if prompt.strip() == "":
+        st.warning("Please enter a prompt.")
+    else:
+        with st.spinner("Thinking..."):
+            response = client.chat(
+                model="gemma3:270m",
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )
+
+            st.success("Response Generated!")
+            st.write(response["message"]["content"])
